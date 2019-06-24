0bad510d9f9b4e1fa3adbb7dc9dd708907928c84
okno <paul.delafosse@protonmail.com>
2019-06-24 02:04:15 +0200

    WIP: actix rest API

--- movie-night-backend/src/main.rs
+++ movie-night-backend/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate serde_derive;
+use actix_web::{middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
+use futures::Future;
+
+fn index(req: &HttpRequest) -> HttpResponse {
+    HttpResponse::Ok().body("ok")
+}
+
+fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(
+                web::resource("/users")
+                    .route(web::post().to(|| HttpResponse::Ok()))
+                    .route(web::get().to(|| HttpResponse::Ok()))
+                    .route(web::put().to(|| HttpResponse::Ok()))
+                    .route(web::patch().to(|| HttpResponse::Ok()))
+                    .route(web::delete().to(|| HttpResponse::Ok())),
+            )
+            .service(
+                web::resource("/movies")
+                    .route(web::post().to(|| HttpResponse::Ok()))
+                    .route(web::get().to(|| HttpResponse::Ok()))
+                    .route(web::put().to(|| HttpResponse::Ok()))
+                    .route(web::patch().to(|| HttpResponse::Ok()))
+                    .route(web::delete().to(|| HttpResponse::Ok())),
+            )
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
 }
