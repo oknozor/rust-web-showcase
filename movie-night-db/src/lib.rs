4aff51055887946e9fcdc84eb1f7339acfe500a3
okno <paul.delafosse@protonmail.com>
2019-06-28 14:01:06 +0200

    feat(db): add  generic error mapping

--- movie-night-db/src/lib.rs
+++ movie-night-db/src/lib.rs
@@ -2,20 +2,22 @@
 extern crate diesel;
 
 
-use diesel::result::Error as DieselError;
-use std::ops::Deref;
-use diesel::r2d2::PoolError;
+use diesel::r2d2::PooledConnection;
+use diesel::r2d2::ConnectionManager;
 use diesel::pg::PgConnection;
-use diesel::r2d2::{self, ConnectionManager, PooledConnection};
-use users::*; 
+use diesel::r2d2::PoolError;
+use diesel::result::Error as DieselError;
+use std::error::Error;
+use std::ops::Deref;
+use users::*;
 
-/// Just a handy type alias for Postgresql connection pool 
+/// Just a handy type alias for Postgresql connection pool
 pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 type PgPooledConnection = PooledConnection<ConnectionManager<PgConnection>>;
 
-pub mod users;
-mod schema;
 
+mod schema;
+pub mod users;
 /// Build the connnection manager and return the connection pool
 pub fn init_pool(database_url: &str) -> Result<Pool, PoolError> {
     let manager = ConnectionManager::<PgConnection>::new(database_url);
@@ -26,11 +28,18 @@ pub fn get_conn(pool: &Pool) -> Result<PgPooledConnection, &'static str> {
     pool.get().map_err(|_| "Can't get connection")
 }
 
+pub fn all_users(pool: &Pool) -> Result<Vec<User>, String> {
+    User::all(get_conn(pool)?.deref()).map_err(map_db_err)
+}
+
 pub fn user_by_id(user_id: i32, pool: &Pool) -> Result<User, String> {
-    User::by_id(user_id, get_conn(pool)?.deref()).map_err(|err| match err {
-        //TODO: map this to detailed errors and some how map these error to HTTP response codes in the backend
+    User::by_id(user_id, get_conn(pool)?.deref()).map_err(map_db_err)
+}
+
+fn map_db_err(err: DieselError) -> String {
+   match err {
         DieselError::DatabaseError(_kind, _info) => String::from(_info.message()),
-        DieselError::NotFound => String::from("User not found in database"),
-        _ => String::from("unkown error")
-    })
+        DieselError::NotFound => String::from("Resource not found in database"),
+        _ => String::from("Unkown database error"),
+    }
 }
